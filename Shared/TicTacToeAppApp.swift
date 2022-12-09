b6126ef45c357fa1c399d9be8dd6896a3a4f8111
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-08 20:09:59 -0800

    Initial Commit

---
+++ Shared/TicTacToeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAppApp.swift
+//  Shared
+//
+//  Created by user on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
