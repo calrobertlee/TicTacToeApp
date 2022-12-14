36cfc228e30696396f2400aee360724df17d6ac6
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-13 19:14:24 -0800

    Another attempt, different approach.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -2,52 +2,120 @@
 //  ContentView.swift
 //  Shared
 //
-//  Created by user on 12/8/22.
+//  Created by C.R. Lee on 12/8/22.
 //
-
-// Tried to use @Published with class
-// Tried to use @Binding w/ @State (probably still the answer)
-// Tried Array, not sure how to id each of the 9 cards, however.
-// Real solution: requires MVVM perhaps?
 
 import SwiftUI
 
 struct ContentView: View {
-    var cards: Array<String> = ["O","1","2","3","4","5","6","7","8",]
-    var cardCount = 9
+    
+    // Declare the game board and empty array.
+    @State var gameBoard:[[String]] = [["_","_","_"],["_","_","_"],["_","_","_"]]
+
+    @State var turnCounter = 0
+
+    let markerX = "X"
+    let markerO = "O"
+    var gameOver = false
     
     var body: some View {
         VStack {
-            Text("Player O's turn").font(.title)
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))]) { // GridItem insead of numerical number because more control
-                ForEach(cards[0..<cardCount], id: \.self) { cards in
-                    SquareView(content: cards).aspectRatio(2/3, contentMode: .fill)
+            Text("Turn: \(turnCounter)")
+            
+            VStack {
+                if turnCounter % 2 == 0 {
+                    HStack {
+                        Text("\(gameBoard[0][0])").onTapGesture {
+                            gameBoard[0][0] = markerO
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[0][1])").onTapGesture {
+                            gameBoard[0][1] = markerO
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[0][2])").onTapGesture {
+                            gameBoard[0][2] = markerO
+                            turnCounter += 1
+                        }
+                    }
+                    HStack {
+                        Text("\(gameBoard[1][0])").onTapGesture {
+                            gameBoard[1][0] = markerO
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[1][1])").onTapGesture {
+                            gameBoard[1][1] = markerO
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[1][2])").onTapGesture {
+                            gameBoard[1][2] = markerO
+                            turnCounter += 1
+                        }
+                    }
+                    HStack {
+                        Text("\(gameBoard[2][0])").onTapGesture {
+                            gameBoard[2][0] = markerO
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[2][1])").onTapGesture {
+                            gameBoard[2][1] = markerO
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[2][2])").onTapGesture {
+                            gameBoard[2][2] = markerO
+                            turnCounter += 1
+                        }
+                    }
+                } else {
+                    HStack {
+                        Text("\(gameBoard[0][0])").onTapGesture {
+                            gameBoard[0][0] = markerX
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[0][1])").onTapGesture {
+                            gameBoard[0][1] = markerX
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[0][2])").onTapGesture {
+                            gameBoard[0][2] = markerX
+                            turnCounter += 1
+                        }
+                    }
+                    HStack {
+                        Text("\(gameBoard[1][0])").onTapGesture {
+                            gameBoard[1][0] = markerX
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[1][1])").onTapGesture {
+                            gameBoard[1][1] = markerX
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[1][2])").onTapGesture {
+                            gameBoard[1][2] = markerX
+                            turnCounter += 1
+                        }
+                    }
+                    HStack {
+                        Text("\(gameBoard[2][0])").onTapGesture {
+                            gameBoard[2][0] = markerX
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[2][1])").onTapGesture {
+                            gameBoard[2][1] = markerX
+                            turnCounter += 1
+                        }
+                        Text("\(gameBoard[2][2])").onTapGesture {
+                            gameBoard[2][2] = markerX
+                            turnCounter += 1
+                        }
+                    }
                 }
-            }
+                
+            }.font(.system(size: 50))
         }
     }
 }
-
-struct SquareView: View {
-    @State var isOwned = true
-    var content: String
-
-    var body: some View {
-        let shape = RoundedRectangle(cornerRadius: 8)
-        
-        ZStack {
-            if isOwned {
-                shape.fill(.white)
-                shape.stroke(lineWidth: 4)
-                Text(content).font(.title)
-            } else {
-                shape.fill(.white)
-            }
-        }.onTapGesture {
-            isOwned = !isOwned
-        }
-    }
-}
+   
 
 
 
@@ -55,6 +123,31 @@ struct SquareView: View {
 
 
 
+
+
+
+
+
+
+
+
+
+/*
+ We need:
+ 1. Squares in a grid
+ 2. Squares need to keep track of:
+    - If they are owned by a player.
+    - Which player owns them.
+    - If they've been clicked on.
+ 3. A grid which understands the current layout of owned squares.
+ 4. A method for which to switch between Player 1 and Player 2.
+    - This method also needs to alter which squares are handed out based on the current player.
+ 
+ Systems include:
+ - Squares
+ - Grid
+ - Memory
+ */
 
 
 
