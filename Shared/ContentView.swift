6b713ac60242e152f34ddf22353b946da187613e
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-13 19:24:55 -0800

    Working on win condition.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,106 +12,114 @@ struct ContentView: View {
     // Declare the game board and empty array.
     @State var gameBoard:[[String]] = [["_","_","_"],["_","_","_"],["_","_","_"]]
 
+    // Alternate between players.
     @State var turnCounter = 0
 
+    // Mark X or O.
     let markerX = "X"
     let markerO = "O"
+    
+    // Check for win condition.
     var gameOver = false
     
     var body: some View {
         VStack {
-            Text("Turn: \(turnCounter)")
-            
-            VStack {
-                if turnCounter % 2 == 0 {
-                    HStack {
-                        Text("\(gameBoard[0][0])").onTapGesture {
-                            gameBoard[0][0] = markerO
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[0][1])").onTapGesture {
-                            gameBoard[0][1] = markerO
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[0][2])").onTapGesture {
-                            gameBoard[0][2] = markerO
-                            turnCounter += 1
-                        }
-                    }
-                    HStack {
-                        Text("\(gameBoard[1][0])").onTapGesture {
-                            gameBoard[1][0] = markerO
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[1][1])").onTapGesture {
-                            gameBoard[1][1] = markerO
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[1][2])").onTapGesture {
-                            gameBoard[1][2] = markerO
-                            turnCounter += 1
-                        }
-                    }
-                    HStack {
-                        Text("\(gameBoard[2][0])").onTapGesture {
-                            gameBoard[2][0] = markerO
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[2][1])").onTapGesture {
-                            gameBoard[2][1] = markerO
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[2][2])").onTapGesture {
-                            gameBoard[2][2] = markerO
-                            turnCounter += 1
-                        }
-                    }
-                } else {
-                    HStack {
-                        Text("\(gameBoard[0][0])").onTapGesture {
-                            gameBoard[0][0] = markerX
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[0][1])").onTapGesture {
-                            gameBoard[0][1] = markerX
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[0][2])").onTapGesture {
-                            gameBoard[0][2] = markerX
-                            turnCounter += 1
-                        }
-                    }
-                    HStack {
-                        Text("\(gameBoard[1][0])").onTapGesture {
-                            gameBoard[1][0] = markerX
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[1][1])").onTapGesture {
-                            gameBoard[1][1] = markerX
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[1][2])").onTapGesture {
-                            gameBoard[1][2] = markerX
-                            turnCounter += 1
-                        }
-                    }
-                    HStack {
-                        Text("\(gameBoard[2][0])").onTapGesture {
-                            gameBoard[2][0] = markerX
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[2][1])").onTapGesture {
-                            gameBoard[2][1] = markerX
-                            turnCounter += 1
-                        }
-                        Text("\(gameBoard[2][2])").onTapGesture {
-                            gameBoard[2][2] = markerX
-                            turnCounter += 1
-                        }
-                    }
-                }
+            if gameOver == false {
+                Text("Turn: \(turnCounter)")
                 
-            }.font(.system(size: 50))
+                VStack {
+                    if turnCounter % 2 == 0 {
+                        HStack {
+                            Text("\(gameBoard[0][0])").onTapGesture {
+                                gameBoard[0][0] = markerO
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[0][1])").onTapGesture {
+                                gameBoard[0][1] = markerO
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[0][2])").onTapGesture {
+                                gameBoard[0][2] = markerO
+                                turnCounter += 1
+                            }
+                        }
+                        HStack {
+                            Text("\(gameBoard[1][0])").onTapGesture {
+                                gameBoard[1][0] = markerO
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[1][1])").onTapGesture {
+                                gameBoard[1][1] = markerO
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[1][2])").onTapGesture {
+                                gameBoard[1][2] = markerO
+                                turnCounter += 1
+                            }
+                        }
+                        HStack {
+                            Text("\(gameBoard[2][0])").onTapGesture {
+                                gameBoard[2][0] = markerO
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[2][1])").onTapGesture {
+                                gameBoard[2][1] = markerO
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[2][2])").onTapGesture {
+                                gameBoard[2][2] = markerO
+                                turnCounter += 1
+                            }
+                        }
+                    } else {
+                        HStack {
+                            Text("\(gameBoard[0][0])").onTapGesture {
+                                gameBoard[0][0] = markerX
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[0][1])").onTapGesture {
+                                gameBoard[0][1] = markerX
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[0][2])").onTapGesture {
+                                gameBoard[0][2] = markerX
+                                turnCounter += 1
+                            }
+                        }
+                        HStack {
+                            Text("\(gameBoard[1][0])").onTapGesture {
+                                gameBoard[1][0] = markerX
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[1][1])").onTapGesture {
+                                gameBoard[1][1] = markerX
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[1][2])").onTapGesture {
+                                gameBoard[1][2] = markerX
+                                turnCounter += 1
+                            }
+                        }
+                        HStack {
+                            Text("\(gameBoard[2][0])").onTapGesture {
+                                gameBoard[2][0] = markerX
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[2][1])").onTapGesture {
+                                gameBoard[2][1] = markerX
+                                turnCounter += 1
+                            }
+                            Text("\(gameBoard[2][2])").onTapGesture {
+                                gameBoard[2][2] = markerX
+                                turnCounter += 1
+                            }
+                        }
+                    }
+                    
+                }.font(.system(size: 50))
+            } else {
+                Text("Player X wins! 🎉").font(.system(size: 36))
+            }
         }
     }
 }
